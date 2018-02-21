e91716cd5165cc4727ceba5f4d36cd99620e43b1
Ken Snyder <ken@ken.net>
2018-02-20 17:32:07 -0800

    better script template

--- generators/app/templates/src/index.ts
+++ generators/app/templates/src/index.ts
@@ -1,1 +1,8 @@
-
+import { AWSGatewayCallback, IDictionary } from "common-types";
+export function handler(
+  event: IDictionary,
+  context: IDBArrayKey,
+  callback: AWSGatewayCallback
+) {
+  console.log("EVENT\n", JSON.stringify(event, null, 2));
+}
